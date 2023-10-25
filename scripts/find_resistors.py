b26c6a67882ba71ea366284e86f37248ea85ac07
Ty Davis <tydavisxc2019@gmail.com>
2023-10-25 16:18:34 -0600

    Been working on the find_resistors script

--- scripts/find_resistors.py
+++ scripts/find_resistors.py
@@ -1,45 +1,40 @@
 import argparse
 
-RESISTOR_FILE = './resistors.txt'
-RES_VALS = [] # To be filled later
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("target_resistor", type=float, metavar="target-resistor", help="Target resistor value to be found.")
-    parser.add_argument("-r", "--resistor-file", help="Path for file showing list of resistor values.")
+    parser.add_argument("-r", "--resistor-file", default="./resistors.txt", help="Path for file showing list of resistor values.")
     parser.add_argument("-t", "--tolerance", type=float, help="Target tolerance in percent for resistor network to be within target resistor value.", default=1)
     args = parser.parse_args()
 
 
-    if args.resistor_file:
-        try:
-            RES_VALS = get_resistors_from_file(args.resistor_file)
-        except FileNotFoundError as e:
-            print("Error:", e)
-            exit()
-    else:
-        RES_VALS = get_resistors_from_file(RESISTOR_FILE)
+    try:
+        res_vals = get_resistors_from_file(args.resistor_file)
+    except FileNotFoundError as e:
+        print("Error:", e)
+        exit()
         
-    get_prime_pair(args.target_resistor, tol=args.tolerance)
+    get_prime_pair(args.target_resistor, tol=args.tolerance, res_vals=res_vals)
 
-def get_prime_pair(target_val, r1=None, tol=1, count=0):
+def get_prime_pair(target_val, r1=None, tol=1, count=0, res_vals=[]):
     count += 1
-    if count == 10:
+    if count == 40:
         print("Recursive Limit reached")
         return 
 
     vals = []
     for func in [series, parallel, select]:
-        val1_potential = RES_VALS if r1 == None else [r1]
+        val1_potential = res_vals if r1 == None else [r1]
         
         for val1 in val1_potential:
-            for val2 in RES_VALS:
+            for val2 in res_vals:
                 final_val = func(val1, val2)
                 vals.append({
                     'rs': [val1] if func == select else [val1, val2],
                     'val': final_val,
                     'method': func.__name__
                 })
+    
     diffs = [val['val'] - target_val for val in vals]
     diffs_abs = [abs(x) for x in diffs]
     index = diffs_abs.index(min(diffs_abs))
@@ -56,7 +51,7 @@ def get_prime_pair(target_val, r1=None, tol=1, count=0):
     if method == 'select':
         print("Couldn't obtain desired tolerance.")
         return
-    res = get_prime_pair(target_val, vals[index]['val'], count=count, tol=tol)
+    res = get_prime_pair(target_val, vals[index]['val'], count=count, tol=tol, res_vals=res_vals)
     if res == None:
         return
 
@@ -65,20 +60,23 @@ def get_resistors_from_file(resistor_file):
     with open(resistor_file) as res_file:
         for val in res_file.readlines():
             try:
-                RES_VALS.append(float(val.split('#')[0].translate({ord('\n'): None})))
+                res_vals.append(float(val.split('#')[0].translate({ord('\n'): None})))
             except Exception as e:
                 continue
     return res_vals
     
 
-def series(r1, r2):
-    return r1 + r2
+def series(*rs):
+    return sum(rs)
 
-def parallel(r1, r2):
-    return r1*r2/(r1+r2)
+def parallel(*rs):
+    total = 0
+    for r in rs:
+        total += 1 / r
+    return 1 / total
 
-def select(r1, r2):
-    return r1
+def select(*rs):
+    return rs[0]
 
 if __name__ == '__main__':
     main()
