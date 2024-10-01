81e4491fb7d2fa88b2447cd023091e0d17f14cf0
Ty Davis <tydavisxc2019@gmail.com>
2024-09-30 21:22:27 -0600

    Working on all the measured data for the lab writeup

--- 2024/lab02/figures/plot_stuff.py
+++ 2024/lab02/figures/plot_stuff.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
+
 def half_wave_sim():
     data1 = pd.read_csv('./half-wave/half-wave-10pp.csv', sep='\t')
     data2 = pd.read_csv('./half-wave/half-wave-0.5pp.csv', sep='\t')
@@ -63,10 +64,66 @@ def precision():
     plt.tight_layout()
     plt.savefig('./precision/precision_sim.pdf')
 
+def half_wave_data():
+    data1 = pd.read_csv('./half-wave/half_wave_data_10vpp.csv', header=1)
+    data2 = pd.read_csv('./half-wave/half_wave_data_0.5vpp.csv', header=1)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax2 = fig.add_subplot(2, 1, 2)
+
+    ax1.plot(data1['second'], data1['Volt'], label='$V_I$')
+    ax1.plot(data1['second'], data1['Volt.1'], label='$V_O$')
+
+    ax2.plot(data2['second'], data2['Volt'], label='$V_I$')
+    ax2.plot(data2['second'], data2['Volt.1'], label='$V_O$')
+
+    plt.tight_layout()
+    plt.savefig('./half-wave/half-wave-measured.pdf')
+
+def peak_data():
+    data1 = pd.read_csv('./peak/peak_data_47k.csv', header=1)
+    data2 = pd.read_csv('./peak/peak_data_4.7k.csv', header=1)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax2 = fig.add_subplot(2, 1, 2)
+
+    ax1.plot(data1['second'], data1['Volt'], label='$V_I$')
+    ax1.plot(data1['second'], data1['Volt.1'], label='$V_O$')
+
+    ax2.plot(data2['second'], data2['Volt'], label='$V_I$')
+    ax2.plot(data2['second'], data2['Volt.1'], label='$V_O$')
+
+    plt.tight_layout()
+    plt.savefig('./peak/peak_measured.pdf')
+
+def precision_data():
+    data1 = pd.read_csv('./precision/precision_data_10vpp.csv', header=1)
+    data2 = pd.read_csv('./precision/precision_data_0.5vpp.csv', header=1)
+
+    fig = plt.figure()
+    ax1 = fig.add_subplot(2, 1, 1)
+    ax2 = fig.add_subplot(2, 1, 2)
+
+    ax1.plot(data1['second'], data1['Volt'], label='$V_I$')
+    ax1.plot(data1['second'], data1['Volt.1'], label='$V_O$')
+
+    ax2.plot(data2['second'], data2['Volt'], label='$V_I$')
+    ax2.plot(data2['second'], data2['Volt.1'], label='$V_O$')
+
+    plt.tight_layout()
+    plt.savefig('./precision/precision_measured.pdf')
+
 def main():
+    plt.rcParams['axes.prop_cycle'] = plt.cycler(color=['#492365', '#aa989c'])
     half_wave_sim()
     peak_rectifier_sim()
     precision()
+
+    half_wave_data()
+    peak_data()
+    precision_data()
 
 
 if __name__ == "__main__":
